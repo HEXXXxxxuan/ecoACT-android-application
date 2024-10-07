9d529a556b3f730d1797fff3a7043ebdbfee16b1
u7327620 <u7327620@anu.edu.au>
2024-10-08 00:40:01 +1100

    Merged login with MainActivity.java

--- App/app/src/main/java/com/go4/application/MainActivity.java
+++ App/app/src/main/java/com/go4/application/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.go4.application.historical.SuburbHistoricalActivity;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
