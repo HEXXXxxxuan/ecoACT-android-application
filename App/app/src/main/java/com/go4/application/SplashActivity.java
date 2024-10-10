a478626e51058d6006c82e06c2f775fce346180d
u7327620 <u7327620@anu.edu.au>
2024-10-11 01:10:58 +1100

    Refactored permission request, need to add background location request in GPSService

--- App/app/src/main/java/com/go4/application/SplashActivity.java
+++ App/app/src/main/java/com/go4/application/SplashActivity.java
@@ -10,8 +10,19 @@ import android.Manifest;
 import android.util.Log;
 import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -19,28 +30,38 @@ import com.go4.utils.DataFetcher;
 
 /**
  * This activity launches the splashScreen on app launch.
- * <p>
- *     It also implements logic required during startup. Presently loading all historical data.
- * </p>
+ * <p>It also implements logic required during startup. Presently loading all historical data.</p>
  * @author Shawn ?
  * @author Gea
  */
 @SuppressLint("CustomSplashScreen")
 public class SplashActivity extends AppCompatActivity {
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 100;
-    private ProgressBar fetchingBar;
+    private static final String[] permissionsStr = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.POST_NOTIFICATIONS};
+
+    ActivityResultLauncher<String[]> permissionsLauncher = registerForActivityResult(
+        new ActivityResultContracts.RequestMultiplePermissions(), result -> {
+            if(result.containsValue(false)){
+                Toast.makeText(this, "All permissions are required for function", Toast.LENGTH_SHORT).show();
+                Log.d("Debugging", result.keySet().toString());
+                checkPermissions();
+            }
+            else{
+                startNextActivity();
+            }
+        });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        fetchingBar = findViewById(R.id.fetchingProgressBar);
+        ProgressBar fetchingBar = findViewById(R.id.fetchingProgressBar);
         fetchingBar.setMax(113); // only 113 suburbs
-        checkLocationPermission();
         fetchData();
     }
 
     /**
+     * <h1><strong>DOES NOT CHECK REQUIRED PERMISSIONS ARE GRANTED</strong></h1>
      * Called to close {@link SplashActivity} and resume to next class
      * <p>
      *     Changing throughout development but should be {@link MainActivity} next
@@ -53,55 +74,39 @@ public class SplashActivity extends AppCompatActivity {
     }
 
     /**
-     * Fetches historical data records using {@link DataFetcher}
-     *
+     * Calls {@link #startNextActivity()} upon finishing
+     * <p>
+     *     Uses an {@link ActivityResultLauncher} to request permissions and does logic in its callback
+     * </p>
+     */
+    private void checkPermissions(){
+        List<String> toRequest = new ArrayList<>();
+        for(String str : permissionsStr){
+            if(!hasPermissions(str)){
+                toRequest.add(str);
+            }
+        }
+        String[] real = new String[toRequest.size()];
+        for (int i = 0; i < toRequest.size(); i++) {
+            real[i] = toRequest.get(i);
+        }
+        permissionsLauncher.launch(real);
+    }
+
+    private boolean hasPermissions(String perm){
+        return ContextCompat.checkSelfPermission(this, perm) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    /**
+     * <h1><strong>CALLS {@link #startNextActivity()} ONCE FINISHED</strong></h1>
+     * <p>Fetches historical data records using {@link DataFetcher}</p>
      * @author Gea
      */
     private void fetchData() {
         ExecutorService executorService = Executors.newFixedThreadPool(6);
         Handler mainHandler = new Handler(Looper.getMainLooper());
         DataFetcher dataFetcher = new DataFetcher(executorService, mainHandler, 7);
-        dataFetcher.automaticAddRecords(this, "historical_data.csv", fetchingBar, this::startNextActivity);
-    }
-
-    /**
-     * Handles requested permissions for the {@link #checkLocationPermission()} method
-     * @param requestCode The request code passed in
-     * @param permissions The requested permissions. Never null.
-     * @param grantResults The grant results for the corresponding permissions
-     *     which is either {@link android.content.pm.PackageManager#PERMISSION_GRANTED}
-     *     or {@link android.content.pm.PackageManager#PERMISSION_DENIED}. Never null.
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                fetchData();
-                return;
-            }
-            // Fall-through when non-successful
-            Toast.makeText(this, "Location permission is required to proceed.", Toast.LENGTH_SHORT).show();
-            try {
-                wait(2000);
-            }
-            catch (InterruptedException e) {
-                Log.d("Debugging", "InterruptedException during onRequestPermissionResult wait");
-                throw new RuntimeException(e);
-            }
-            checkLocationPermission();
-        }
-    }
-
-    /**
-     * Requests ACCESS_FINE_LOCATION as well as ACCESS_BACKGROUND_LOCATION
-     */
-    private void checkLocationPermission() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
-        }
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_BACKGROUND_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
-        }
+        // dataFetcher.automaticAddRecords(this, "historical_data.csv", fetchingBar, this::checkPermissions);
+        checkPermissions();
     }
 }
