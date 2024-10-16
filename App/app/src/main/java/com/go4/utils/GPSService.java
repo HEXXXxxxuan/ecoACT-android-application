939cec54dd5bdffda1f2f83c6c56269a04ea931a
u7327620 <u7327620@anu.edu.au>
2024-10-16 20:07:01 +1100

    Docstring and clean GPSService.java

--- App/app/src/main/java/com/go4/utils/GPSService.java
+++ App/app/src/main/java/com/go4/utils/GPSService.java
@@ -1,6 +1,7 @@
 package com.go4.utils;
 
 import static com.go4.application.MainActivity.CHANNEL_ID;
+import static com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY;
 
 import android.Manifest;
 import android.app.NotificationManager;
@@ -15,24 +16,33 @@ import android.util.Log;
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import com.go4.application.R;
-import com.google.android.gms.location.CurrentLocationRequest;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.CancellationTokenSource;
-import com.google.android.gms.tasks.OnSuccessListener;
 
+/**
+ * Extends {@link Service} to provide an API interface to retrieve location data from.
+ * <p>Intended as a bound-service through an implementation of
+ * {@link android.content.ServiceConnection} where <code>gps = binder.getService()</code></p>
+ * <p>Retrieve location with {@link #getRecentLocation()}</p>
+ *
+ * @author Ryan Foote
+ */
 public class GPSService extends Service {
     private final IBinder binder = new LocalBinder();
     private FusedLocationProviderClient locationClient;
-    private LocationRequest request;
     private LocationCallback locationCallback;
-    private static Location latestLocation;
-    private static Location previousLocation = null;
+    private Location latestLocation;
     private NotificationManager notificationManager;
 
+    /**
+     * Extends {@link Binder} with a single method {@link #getService()} to return the surrounding
+     * {@link GPSService} instance.
+     *
+     * @author Ryan Foote
+     */
     public class LocalBinder extends Binder {
         public GPSService getService(){
             return GPSService.this;
@@ -49,43 +59,32 @@ public class GPSService extends Service {
     public void onCreate(){
         super.onCreate();
         locationClient = LocationServices.getFusedLocationProviderClient(this);
-        request = createLocationRequest();
         notificationManager = (NotificationManager) getSystemService(Service.NOTIFICATION_SERVICE);
-
         locationCallback = new LocationCallback() {
             @Override
             public void onLocationResult(LocationResult locationResult) {
-                Log.d("Debugging", "Location received");
                 if (locationResult == null || locationResult.getLastLocation() == null) {
                     locationNotification("Location not available");
-                    return;
                 }
-
-                latestLocation = locationResult.getLastLocation();
-                Log.d("Debugging", "Lat: " + latestLocation.getLatitude() + ", Long: " + latestLocation.getLongitude());
-
-
-                if (previousLocation != null && latestLocation.distanceTo(previousLocation) < 1) {
-                    Log.d("Debugging", "Location unchanged, not updating");
-                } else {
-                    previousLocation = latestLocation;
-                    locationNotification("Updated Location: Lat = " + latestLocation.getLatitude() + ", Long = " + latestLocation.getLongitude());
+                else{
+                    latestLocation = locationResult.getLastLocation();
+                    locationNotification("Current Location: Lat=" + latestLocation.getLatitude() + ", Long=" + latestLocation.getLongitude());
                 }
             }
         };
-
-        startLocationUpdates();
+        LocationRequest request = new LocationRequest.Builder(PRIORITY_HIGH_ACCURACY, 5000).build();
+        startLocationUpdates(request, locationCallback);
     }
 
-    protected LocationRequest createLocationRequest() {
-        return new LocationRequest.Builder(
-                LocationRequest.PRIORITY_HIGH_ACCURACY, 5000)
-                .build();
-    }
-
+    /**
+     * Helper for {@link NotificationCompat.Builder}
+     * @param text for {@link NotificationCompat.Builder#setContentText}
+     *
+     * @author Ryan Foote
+     */
     private void locationNotification(String text) {
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentTitle("Location Results")
+                .setContentTitle("Location Retrieved")
                 .setContentText(text)
                 .setSmallIcon(R.drawable.gp_logo)
                 .setAutoCancel(true)
@@ -100,22 +99,27 @@ public class GPSService extends Service {
         super.onDestroy();
     }
 
-    private void startLocationUpdates() {
+    private void startLocationUpdates(LocationRequest request, LocationCallback locationCallback) {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Log.d("Debugging", "Not enough permissions, requesting");
+            ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d("Debugging", "Not enough permissions");
             return;
         }
         locationClient.requestLocationUpdates(request, locationCallback, Looper.getMainLooper());
     }
 
+    /**
+     * <strong>Returns a mock location (Ainslie) if no location has been retrieved yet.</strong>
+     * @return {@link #latestLocation}
+     *
+     * @author Ryan Foote
+     */
     public Location getRecentLocation() {
         if (latestLocation == null) {
-            Log.d("Debugging", "No stored latestLocation, giving default location (Ainslie)");
+            Log.d("Debugging", "latest location is null, returning mock location (Ainslie)");
             Location location = new Location("");
             location.setLatitude(-35.27944d);
             location.setLongitude(149.11944d);
-            previousLocation = location;
             return location;
         }
         return latestLocation;
